f8b438108762fe2ea7096e1dcd605fadf2ef5e95
Gurkan Sahin <gurkansahin08@gmail.com>
2019-07-03 13:41:19 +0300

    güncelleme

--- binary_image_classification/Loss.py
+++
@@ -1,9 +0,0 @@
-from torch.nn import BCELoss
-
-class Loss(object):
-    
-    def get_loss(loss_idx):
-        if loss_idx==1:
-        	return BCELoss()
-       
-
--- binary_image_classification/Optimizer.py
+++
@@ -1,9 +0,0 @@
-from torch import optim
-
-class Optimizer(object):
-    
-    def get_optimizer(opt_idx,net,lr):
-        if opt_idx==1:
-            return optim.Adam(net.parameters(),lr=lr)
-        if opt_idx==2:
-            return optim.SGD(net.parameters(),lr=lr,momentum=0.9)
